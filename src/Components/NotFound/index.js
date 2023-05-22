c72293f7f4758496026f012cc21a3de303020026
neel204 <neel.chavda204@gmail.com>
2023-05-22 23:45:10 +0530

    added

--- src/Components/NotFound/index.js
+++ src/Components/NotFound/index.js
@@ -1,5 +1,19 @@
-const NotFound = () => (
-    <h1>not found</h1>
-)
+import {Link} from 'react-router-dom'
+import './index.css';
 
-export default NotFound
+const NotFound = () => (
+  <div className="not-found-container">
+    <img
+      src="http://res.cloudinary.com/dqhwxowdo/image/upload/c_scale,h_1561/v1684779234/2446912_nyveun.jpg"
+      alt='not-found'
+      className="not-found-img"
+    />
+    <h1>Error 404</h1>
+    <h3>The page you looking for is not found.</h3>
+    <Link to='/'>
+        <button>To Home</button>
+    </Link>
+  </div>
+);
+
+export default NotFound;
