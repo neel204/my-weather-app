55068719efe037681a4a8f0ceff83257453b23d5
neel204 <neel.chavda204@gmail.com>
2023-05-24 14:34:27 +0530

    added

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -1,18 +1,18 @@
 import './index.css'
 
 const Cards = () => (
-    <div class="card-container">
-        <div class="card">
-        <div class="card-date">April 27, 2023</div>
-        <div class="card-temp">
+    <div className="card-container">
+        <div className="card">
+        <div className="card-date">April 27, 2023</div>
+        <div className="card-temp">
             <div>
                 <h1>32<sup>o</sup></h1>
                 <h1 >Rain</h1>
             </div>
         </div>
-        <div class="details-container">
-            <div class="detail">
-                <div class="temp-info">
+        <div className="details-container">
+            <div className="detail">
+                <div className="temp-info">
                     <div>
                         <h1>Temp</h1>
                         <div>
@@ -30,15 +30,15 @@ const Cards = () => (
                 </div>
                 <hr/>
                 </div>
-                <div class="detail">
-                    <div class="rain-detail">
+                <div className="detail">
+                    <div className="rain-detail">
                         <h1>Chances of Rain</h1>
                         <p>0%</p>
                     </div>
                 </div>
                 <hr/>
-                <div class="detail">
-                    <div class="sun-time-container">
+                <div className="detail">
+                    <div className="sun-time-container">
                         <div>
                             <h2>Sunrise</h2>
                             <p>06:34 AM</p>
@@ -51,192 +51,201 @@ const Cards = () => (
                 </div>
         </div>
         </div>
-        <div class="card">
-        <div class="card-date">April 27, 2023</div>
-        <div class="card-temp">
+        <div className="card">
+        <div className="card-date">April 27, 2023</div>
+        <div className="card-temp">
             <div>
                 <h1>32<sup>o</sup></h1>
-                <h1>Snow</h1>
+                <h1 >Rain</h1>
             </div>
         </div>
-        <div class="details-container">
-            <div class="detail">
-                <div class="temp-info">
+        <div className="details-container">
+            <div className="detail">
+                    <hr></hr>
+                <div className="temp-info">
                     <div>
                         <h1>Temp</h1>
                         <div>
-                            <p>max 35.5<sup>o</sup></p>
-                            <p>min 35.5<sup>o</sup></p>
+                            <p>Max 35.5<sup>o</sup></p>
+                            <p>Min 35.5<sup>o</sup></p>
                         </div>
                     </div>
                     <div>
                         <h1>Wind</h1>
                         <div>
-                            <p>max 35.5<sup>o</sup></p>
-                            <p>min 35.5<sup>o</sup></p>
+                            <p>Max 35.5<sup>o</sup></p>
+                            <p>Min 35.5<sup>o</sup></p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="detail">
-                <div class="rain-detail">
-                    <h1>Chances of Rain</h1>
-                    <p>0%</p>
+                <hr/>
                 </div>
-            </div>
-            <div class="detail">
-                <div class="sun-time-container">
-                    <div>
-                        <h2>Sunrise</h2>
-                        <p>06:34 AM</p>
-                    </div>
-                    <div>
-                        <h2>Sunset</h2>
-                        <p>06:34 AM</p>
+                <div className="detail">
+                    <div className="rain-detail">
+                        <h1>Chances of Rain</h1>
+                        <p>0%</p>
                     </div>
                 </div>
-            </div>
+                <hr/>
+                <div className="detail">
+                    <div className="sun-time-container">
+                        <div>
+                            <h2>Sunrise</h2>
+                            <p>06:34 AM</p>
+                        </div>
+                        <div>
+                            <h2>Sunset</h2>
+                            <p>06:34 AM</p>
+                        </div>
+                    </div>
+                </div>
         </div>
         </div>
-        <div class="card">
-        <div class="card-date">April 27, 2023</div>
-        <div class="card-temp">
+        <div className="card">
+        <div className="card-date">April 27, 2023</div>
+        <div className="card-temp">
             <div>
                 <h1>32<sup>o</sup></h1>
-                <h1>Sunny</h1>
+                <h1 >Rain</h1>
             </div>
         </div>
-        <div class="details-container">
-            <div class="detail">
-                <div class="temp-info">
+        <div className="details-container">
+            <div className="detail">
+                <div className="temp-info">
                     <div>
                         <h1>Temp</h1>
                         <div>
-                            <p>max 35.5<sup>o</sup></p>
-                            <p>min 35.5<sup>o</sup></p>
+                            <p>Max 35.5<sup>o</sup></p>
+                            <p>Min 35.5<sup>o</sup></p>
                         </div>
                     </div>
                     <div>
                         <h1>Wind</h1>
                         <div>
-                            <p>max 35.5<sup>o</sup></p>
-                            <p>min 35.5<sup>o</sup></p>
+                            <p>Max 35.5<sup>o</sup></p>
+                            <p>Min 35.5<sup>o</sup></p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="detail">
-                <div class="rain-detail">
-                    <h1>Chances of Rain</h1>
-                    <p>0%</p>
+                <hr/>
                 </div>
-            </div>
-            <div class="detail">
-                <div class="sun-time-container">
-                    <div>
-                        <h2>Sunrise</h2>
-                        <p>06:34 AM</p>
-                    </div>
-                    <div>
-                        <h2>Sunset</h2>
-                        <p>06:34 AM</p>
+                <div className="detail">
+                    <div className="rain-detail">
+                        <h1>Chances of Rain</h1>
+                        <p>0%</p>
                     </div>
                 </div>
-            </div>
+                <hr/>
+                <div className="detail">
+                    <div className="sun-time-container">
+                        <div>
+                            <h2>Sunrise</h2>
+                            <p>06:34 AM</p>
+                        </div>
+                        <div>
+                            <h2>Sunset</h2>
+                            <p>06:34 AM</p>
+                        </div>
+                    </div>
+                </div>
         </div>
         </div>
-        <div class="card">
-        <div class="card-date">April 27, 2023</div>
-        <div class="card-temp">
+        <div className="card">
+        <div className="card-date">April 27, 2023</div>
+        <div className="card-temp">
             <div>
                 <h1>32<sup>o</sup></h1>
-                <h1>Sunny</h1>
+                <h1 >Rain</h1>
             </div>
         </div>
-        <div class="details-container">
-            <div class="detail">
-                <div class="temp-info">
+        <div className="details-container">
+            <div className="detail">
+                <div className="temp-info">
                     <div>
                         <h1>Temp</h1>
                         <div>
-                            <p>max 35.5<sup>o</sup></p>
-                            <p>min 35.5<sup>o</sup></p>
+                            <p>Max 35.5<sup>o</sup></p>
+                            <p>Min 35.5<sup>o</sup></p>
                         </div>
                     </div>
                     <div>
                         <h1>Wind</h1>
                         <div>
-                            <p>max 35.5<sup>o</sup></p>
-                            <p>min 35.5<sup>o</sup></p>
+                            <p>Max 35.5<sup>o</sup></p>
+                            <p>Min 35.5<sup>o</sup></p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="detail">
-                <div class="rain-detail">
-                    <h1>Chances of Rain</h1>
-                    <p>0%</p>
+                <hr/>
                 </div>
-            </div>
-            <div class="detail">
-                <div class="sun-time-container">
-                    <div>
-                        <h2>Sunrise</h2>
-                        <p>06:34 AM</p>
-                    </div>
-                    <div>
-                        <h2>Sunset</h2>
-                        <p>06:34 AM</p>
+                <div className="detail">
+                    <div className="rain-detail">
+                        <h1>Chances of Rain</h1>
+                        <p>0%</p>
                     </div>
                 </div>
-            </div>
+                <hr/>
+                <div className="detail">
+                    <div className="sun-time-container">
+                        <div>
+                            <h2>Sunrise</h2>
+                            <p>06:34 AM</p>
+                        </div>
+                        <div>
+                            <h2>Sunset</h2>
+                            <p>06:34 AM</p>
+                        </div>
+                    </div>
+                </div>
         </div>
         </div>
-        <div class="card">
-        <div class="card-date">April 27, 2023</div>
-        <div class="card-temp">
+        <div className="card">
+        <div className="card-date">April 27, 2023</div>
+        <div className="card-temp">
             <div>
-                <h1 class="temp">32<sup>o</sup></h1>
-                <h1 class="condition">Sunny</h1>
+                <h1>32<sup>o</sup></h1>
+                <h1 >Rain</h1>
             </div>
         </div>
-        <div class="details-container">
-            <div class="detail">
-                <div class="temp-info">
+        <div className="details-container">
+            <div className="detail">
+                <div className="temp-info">
                     <div>
-                        <h1 class="card-h1">Temp</h1>
-                        <div class="card-p">
-                            <p>max 35.5<sup>o</sup></p>
-                            <p>min 35.5<sup>o</sup></p>
+                        <h1>Temp</h1>
+                        <div>
+                            <p>Max 35.5<sup>o</sup></p>
+                            <p>Min 35.5<sup>o</sup></p>
                         </div>
                     </div>
                     <div>
-                        <h1 class="card-h1">Wind</h1>
-                        <div class="card-p">
-                            <p>max 35.5<sup>o</sup></p>
-                            <p>min 35.5<sup>o</sup></p>
+                        <h1>Wind</h1>
+                        <div>
+                            <p>Max 35.5<sup>o</sup></p>
+                            <p>Min 35.5<sup>o</sup></p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="detail">
-                <div class="rain-detail">
-                    <h1 class="card-h1">Chances of Rain</h1>
-                    <p class="card-p">0%</p>
+                <hr/>
                 </div>
-            </div>
-            <div class="detail">
-                <div class="sun-time-container">
-                    <div>
-                        <h2 class="card-h1">Sunrise</h2>
-                        <p class="caed-p">06:34 AM</p>
-                    </div>
-                    <div>
-                        <h2 class="card-h1">Sunset</h2>
-                        <p class="card-p">06:34 AM</p>
+                <div className="detail">
+                    <div className="rain-detail">
+                        <h1>Chances of Rain</h1>
+                        <p>0%</p>
                     </div>
                 </div>
-            </div>
+                <hr/>
+                <div className="detail">
+                    <div className="sun-time-container">
+                        <div>
+                            <h2>Sunrise</h2>
+                            <p>06:34 AM</p>
+                        </div>
+                        <div>
+                            <h2>Sunset</h2>
+                            <p>06:34 AM</p>
+                        </div>
+                    </div>
+                </div>
         </div>
         </div>
     </div>
