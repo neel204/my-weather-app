5efaca1353be21fb97b3634abd3202de12b95a8a
neel204 <neel.chavda204@gmail.com>
2023-05-25 13:05:41 +0530

    added

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -8,39 +8,39 @@ const Footer = () => (
     <>
     <div className='footer-container-1'>
         <div className='footer-info-container'>
-            <div>
-            <h1>Weather By</h1>
-            <p>Stay informed and prepared with our weather website. From sunny skies to severe weather warnings, we've got you covered.</p>
+            <div className='f-container-1'>
+            <h1 className='footer-1-heading'>Weather By</h1>
+            <p className='footer-1-para'>Stay informed and prepared with our weather website. From sunny skies to severe weather warnings, we've got you covered.</p>
             </div>
-            <div>
-            <a target="_blank" href="https://www.instagram.com/accounts/login/" rel="noreferrer">
+            <div className='social-icons'>
+            <a className='each-icon' target="_blank" href="https://www.instagram.com/accounts/login/" rel="noreferrer">
                 <BsInstagram/>
             </a>
-            <a target="_blank" href='https://www.facebook.com/login/' rel="noreferrer">
+            <a className='each-icon' target="_blank" href='https://www.facebook.com/login/' rel="noreferrer">
                 <BsFacebook/>
             </a>
-            <a target="_blank" href='https://twitter.com/login?lang=en' rel="noreferrer">
+            <a className='each-icon' target="_blank" href='https://twitter.com/login?lang=en' rel="noreferrer">
                 <BsTwitter/>
             </a>
             </div>
         </div>
-        <div>
-            <h1>About Us</h1>
-            <p>Contact Us</p>
-            <p>Who am I?</p>
-            <p>FAQ</p>
-            <p>Help</p>
+        <div className='f-container-2'>
+            <h1 className='footer-1-heading'>About Us</h1>
+            <p className='footer-1-para'>Contact Us</p>
+            <p className='footer-1-para'>Who am I?</p>
+            <p className='footer-1-para'>FAQ</p>
+            <p className='footer-1-para'>Help</p>
         </div>
-        <div>
-            <h1>FeedBack</h1>
-            <p>About API</p>
-            <p>Features</p>
-            <p>Bugs</p>
+        <div className='f-container-3'>
+            <h1 className='footer-1-heading'>FeedBack</h1>
+            <p className='footer-1-para'>About API</p>
+            <p className='footer-1-para'>Features</p>
+            <p className='footer-1-para'>Bugs</p>
         </div>
     </div>
     <div className='footer-container-2'>
-        <BiCopyright className=''/>
-        <p>CopyRight WeatherBy. Api by M3o.com</p>
+        <BiCopyright className='copy-icon'/>
+        <p className='footer-2-para'>CopyRight WeatherBy. Api by M3o.com</p>
     </div>
     </>
 )
